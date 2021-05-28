ed666112a90830eb7c414113d6e74b67a66c69fd
Kvitkovsky Maxim <kvitkovsky1302@gmail.com>
2021-05-28 18:35:17 +0300

    выполненное основное задание

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ import galleryItems from './gallery-items.js';
 
 const refs = {
     listGallery: document.querySelector('.js-gallery'),
+    lightBoxGallery: document.querySelector('.js-lightbox'),
+    lightBoxImage: document.querySelector('.lightbox__image'),
+    closeButton: document.querySelector('button[data-action="close-lightbox"]'),
+    imageGallery: document.querySelectorAll('.gallery__image'),
 }
 
 const createGallery = galleryItems.map(({ preview, description, original }) => {
@@ -13,4 +17,16 @@ refs.listGallery.querySelectorAll('li').forEach(item => item.classList.add('gall
 refs.listGallery.querySelectorAll('li a').forEach(item => item.classList.add('gallery__link'));
 refs.listGallery.querySelectorAll('li a img').forEach(item => item.classList.add('gallery__image'));
 
+refs.listGallery.addEventListener('click', onClickImageGallery);
+refs.closeButton.addEventListener('click', onCloseLightBox);
 
+function onClickImageGallery(event) {
+    refs.lightBoxGallery.classList.add('is-open');
+    refs.lightBoxImage.src = event.target.dataset.source;
+    refs.lightBoxImage.alt = event.target.alt;
+};
+
+function onCloseLightBox() {
+    refs.lightBoxGallery.classList.remove('is-open');
+    refs.lightBoxImage.src = '';
+} 
