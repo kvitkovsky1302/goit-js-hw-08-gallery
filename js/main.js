36ec648499ef3362d0c1f9118edb80f1f898d8c1
Kvitkovsky Maxim <kvitkovsky1302@gmail.com>
2021-05-29 18:07:59 +0300

    добавляет перелистывание фото с помощью стрелочек

--- js/main.js
+++ js/main.js
@@ -20,12 +20,17 @@ refs.listGallery.querySelectorAll('li a img').forEach(item => item.classList.add
 refs.listGallery.addEventListener('click', onClickImageGallery);
 refs.closeButton.addEventListener('click', onCloseLightBox);
 
+const listOfImages = galleryItems.map(({ original }) => original);
+
+
 function onClickImageGallery(event) {
     event.preventDefault();
     refs.lightBoxGallery.classList.add('is-open');
     refs.lightBoxImage.src = event.target.dataset.source;
     refs.lightBoxImage.alt = event.target.alt;
     window.addEventListener('keydown', onPressEsc);
+    window.addEventListener('keydown', onThroughtRight);
+    window.addEventListener('keydown', onThroughtLeft);
     refs.lightBoxOverlay.addEventListener('click', onCloseLightBox);
 };
 
@@ -33,11 +38,37 @@ function onPressEsc(event) {
     if (event.code === 'Escape') {
         onCloseLightBox();
         window.removeEventListener('keydown', onPressEsc);
+        window.removeEventListener('keydown', onThroughtRight);
+        window.removeEventListener('keydown', onThroughtLeft);
+        refs.lightBoxOverlay.removeEventListener('click', onCloseLightBox);
     }
-}
+};
+
+function onThroughtRight(event) {
+    if (event.code === 'ArrowRight') {
+        let indexOfImage = listOfImages.indexOf(refs.lightBoxImage.src);
+        if (indexOfImage === listOfImages.length - 1) {
+            indexOfImage = -1;
+        }
+        refs.lightBoxImage.src = listOfImages[indexOfImage + 1];
+    }
+};
+
+function onThroughtLeft(event) {
+    if (event.code === 'ArrowLeft') {
+        let indexOfImage = listOfImages.indexOf(refs.lightBoxImage.src);
+        if (indexOfImage === 0) {
+            indexOfImage = listOfImages.length;
+        }
+        refs.lightBoxImage.src = listOfImages[indexOfImage - 1];
+    }
+};
 
 function onCloseLightBox() {
     refs.lightBoxGallery.classList.remove('is-open');
     refs.lightBoxImage.src = '';
+    window.removeEventListener('keydown', onPressEsc);
+    window.removeEventListener('keydown', onThroughtRight);
+    window.removeEventListener('keydown', onThroughtLeft);
     refs.lightBoxOverlay.removeEventListener('click', onCloseLightBox);
-} 
+};
